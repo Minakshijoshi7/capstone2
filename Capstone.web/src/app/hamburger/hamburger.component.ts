ee553c9ebf190b70957a69856da8715adf9b0acd
student-avd <kaurm437@mymacewan.ca>
2024-07-03 17:51:34 -0600

    created hamburger component

---
+++ Capstone.web/src/app/hamburger/hamburger.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hamburger',
+  templateUrl: './hamburger.component.html',
+  styleUrl: './hamburger.component.css'
+})
+export class HamburgerComponent {
+
+}
