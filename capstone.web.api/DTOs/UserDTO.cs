30bc358055c6988e71b3c30d429c58324ea7a670
Gpreetca <144976919+Gpreetca@users.noreply.github.com>
2024-07-17 14:40:24 -0600

    Added User and Notes DTOs

---
+++ capstone.web.api/DTOs/UserDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace capstone.web.api.DTOs
+{
+    public class UserDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Username { get; set; }
+    }
+}
--- capstone.web.api/obj/Release/net8.0/capstone.web.api.AssemblyInfo.cs
+++ capstone.web.api/obj/Release/net8.0/capstone.web.api.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("capstone.web.api")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bef775c1c134f835002950384398695656116c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("capstone.web.api")]
